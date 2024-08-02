d041780c3fc9fb5f5fd3967b55551f185483bfaa
WaPoNe <michele.fantetti@gmail.com>
2024-08-02 16:02:40 +0200

    A specific log file by di.xml

--- Logger/CustomLogger.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace Gene\EncryptionKeyManager\Logger;
-
-use Magento\Framework\Logger\Monolog;
-
-class CustomLogger extends Monolog
-{
-}
--- Plugin/LogDecrypts.php
+++ Plugin/LogDecrypts.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace Gene\EncryptionKeyManager\Plugin;
 
-use Gene\EncryptionKeyManager\Logger\CustomLogger;
+use Psr\Log\LoggerInterface;
 use Magento\Framework\Encryption\Encryptor;
 use Magento\Framework\App\DeploymentConfig;
 
@@ -20,14 +20,14 @@ class LogDecrypts
     /**
      * @param Encryptor $encryptor
      * @param DeploymentConfig $deploymentConfig
-     * @param CustomLogger $logger
+     * @param LoggerInterface $logger
      * @throws \Magento\Framework\Exception\FileSystemException
      * @throws \Magento\Framework\Exception\RuntimeException
      */
     public function __construct(
         Encryptor $encryptor,
         DeploymentConfig $deploymentConfig,
-        private readonly CustomLogger $logger
+        private readonly LoggerInterface $logger
     ) {
         $this->keyCount = count(explode(PHP_EOL, $encryptor->exportKeys())) - 1;
 
